f2e3420f13e8f75b6a1db93b0e71dd7d5df7d20f
Xunle <1601315809@qq.com>
2021-11-24 23:10:16 +0800

    生产环境

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,18 @@
 module.exports = {
-
+  devServer: {
+    port: 9000,
+    proxy: {
+      '/': {
+        target: 'localhost:8000',
+        changeOrigin: true,
+        pathRewrite: {
+          '^/': ''
+        }
+      }
+    }
+  },
+  publicPath: './',
+  assetsDir: 'static',
   transpileDependencies: [
     'vuetify'
   ],
